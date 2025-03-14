ffd81e09875ddfccc771db0608adb4c9a2b5d106
Lilliana Marrero <marrerosolis@gmail.com>
2025-03-14 15:34:15 -0400

    remove file names for build files

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,19 +15,7 @@ export default defineConfig(({ mode }) => {
     base: mode === 'production' ? './' : '/',
     build: {
       outDir: 'dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          entryFileNames: 'assets/buildIndex[name].js',
-          chunkFileNames: 'assets/buildChunk[name].js',
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name.endsWith('.css')) {
-              return 'assets/buildStyles.css';
-            }
-            return 'assets/[name][extname]';
-          }
-        }
-      }
+      emptyOutDir: true
     }
   }
 })
